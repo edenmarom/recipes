d1791b0eca67ab1dcd99e644d6411cecc9774c0d
DianaAlankry <didushka95@gmail.com>
2023-01-12 14:50:11 +0200

    Login
    firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/App.css';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -8,23 +8,60 @@ import Admin from "./components/Admin";
 import MyPage from './components/MyPage'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 // import Auth from './Auth';
+import AuthForm from './components/AuthForm';
+import { auth } from "./Base";
 
 // const auth = new Auth()
+// var userDetailContext = React.createContext(null);   
+const logOutUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBvsP0fDH9aLU8VIGZrrj4ik75P8UmqFqs';
+
+;
+
+
+
+
 
 
 function App() {
+
+  var [useDeatials, setUserDetails] = useState({
+    name: "",
+    isLogin: false
+  });
+
+
+  const logout = () => {
+    fetch(logOutUrl,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => {
+        return res.json();
+      }).then(res => {
+        console.log('signout => ', res);
+        setUserDetails({ name: '', isLogin: false })
+      }).catch(err => {
+        console.log('err.message =>', err.message);
+      });
+  }
+
   return (
-      <div className="App">
-        <Header />
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Main message="Welcome to our recipes website!" showbutton="true" />
-            </Route>
-            <Route exact path="/about">
-              <Main message="Thank you for visiting us!" showbutton="false" />
-            </Route>
-            {/* <Route exact path="/callback">
+    <div className="App">
+      <Header isLogin={useDeatials.isLogin} logout={logout} />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Main message="Welcome to our recipes website!" showbutton="true" />
+          </Route>
+          <Route exact path="/about">
+            <Main message="Thank you for visiting us!" showbutton="false" />
+          </Route>
+          {/* <Route exact path="/callback">
           <Callback />
       </Route> */}
             {/* <Route exact path="/notfound">
@@ -36,6 +73,9 @@ function App() {
             <Route exact path="/admin">
               <Admin showbutton="false" />
             </Route>
+          <Route exact path="/login">
+            <AuthForm showbutton="false" setUserDetails={setUserDetails} />
+          </Route>
             <Route exact path="/myPage">
               <MyPage/>
             </Route>
@@ -49,3 +89,4 @@ function App() {
 }
 
 export default App;
+//test
