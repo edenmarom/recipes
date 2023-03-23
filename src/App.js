276a56b6b8c898f86d41c9f4fc04dea48c4b2898
Peleg Admi <pelegadmi@gmail.com>
2023-03-23 20:28:13 +0200

    fixed userCount

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './css/App.css';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -6,75 +6,103 @@ import Recipes from './components/Recipes';
 import Admin from "./components/Admin";
 import MyPage from './components/MyPage'
 import UserProfile from './components/Profile/UserProfile'
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import AuthForm from './components/AuthForm';
-import { AuthContextProvider } from './store/AuthContext'
+import {AuthContextProvider} from './store/AuthContext'
+import io from "socket.io-client";
+import {serverSocketAddress} from "./utils/http-communication";
 
-var userDetailContext = React.createContext(null);   
+var userDetailContext = React.createContext(null);
 const logOutUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBvsP0fDH9aLU8VIGZrrj4ik75P8UmqFqs';
+
+
+function socketInitializtion(setConnectedUsers) {
+    const socket = io(serverSocketAddress);
+    socket.on("counter", (counter) => {
+        setConnectedUsers(counter);
+    });
+    socket.on("connect_error", (error) => {
+        setConnectedUsers("Error");
+        console.log("WS Error: Cannot connect to server");
+    });
+    return () => {
+        socket.disconnect();
+    };
+}
+
 
 function App() {
 
-  var [useDeatials, setUserDetails] = useState({
-    name: "",
-    isLogin: false
-  });
+    const [connectedUsers, setConnectedUsers] = useState(0);
 
-  const logout = () => {
-    fetch(logOutUrl,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => {
-        return res.json();
-      }).then(res => {
-        console.log('signout => ', res);
-        setUserDetails({ name: '', isLogin: false })
-      }).catch(err => {
-        console.log('err.message =>', err.message);
-      });
-  }
+    useEffect(() => {
+        socketInitializtion(setConnectedUsers);
+    }, []);
 
-  return (
-    <div className="App">
-        <AuthContextProvider>
-      <BrowserRouter>
-      <Header isLogin={useDeatials.isLogin} logout={logout} />
 
-        <Routes>
-          <Route path="/">
-            <Route
-              index
-              element={
-                <Main
-                  message="Welcome to our recipes website!"
-                  showbutton="true"
-                />
-              }
-            />
-            <Route
-              path="about"
-              element={
-                <Main message="Thank you for visiting us!" showbutton="false" />
-              }
-            />
-            <Route path="recipes" element={<Recipes showbutton="false" />} />
-            <Route path="admin" element={<Admin showbutton="false" />} />
-            <Route path="myPage" element={<MyPage />} />
-            {useDeatials.isLogin && <Route path="UserProfile" element={<UserProfile />} />}
-            <Route exact path="login" element={<AuthForm showbutton="false" setUserDetails={setUserDetails} />}>
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      </AuthContextProvider>
-    </div>
-  );
+    var [useDeatials, setUserDetails] = useState({
+        name: "",
+        isLogin: false
+    });
+
+    const logout = () => {
+        fetch(logOutUrl,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    returnSecureToken: true
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => {
+            return res.json();
+        }).then(res => {
+            console.log('signout => ', res);
+            setUserDetails({name: '', isLogin: false})
+        }).catch(err => {
+            console.log('err.message =>', err.message);
+        });
+    }
+
+    return (
+        <div className="App">
+            <AuthContextProvider>
+                <BrowserRouter>
+                    <Header isLogin={useDeatials.isLogin} logout={logout}/>
+
+                    <Routes>
+                        <Route path="/">
+                            <Route
+                                index
+                                element={
+                                    <Main
+                                        message="Welcome to our recipes website!"
+                                        showbutton="true"
+                                    />
+                                }
+                            />
+                            <Route
+                                path="about"
+                                element={
+                                    <Main message="Thank you for visiting us!" showbutton="false"/>
+                                }
+                            />
+                            <Route path="recipes" element={<Recipes showbutton="false"/>}/>
+                            <Route path="admin" element={<Admin connectedUsers={connectedUsers} showbutton="false"/>}/>
+                            <Route path="myPage" element={<MyPage/>}/>
+                            {useDeatials.isLogin && <Route path="UserProfile" element={<UserProfile/>}/>}
+                            <Route exact path="login"
+                                   element={<AuthForm showbutton="false" setUserDetails={setUserDetails}/>}>
+                            </Route>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </AuthContextProvider>
+            <script src="/socket.io/socket.io.js"></script>
+
+        </div>
+    );
 }
 
 export default App;
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,56 +1,38 @@
 import "../css/Admin.css";
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import io from "socket.io-client";
-import { serverSocketAddress } from "../utils/http-communication.js";
-import { useSelector } from "react-redux";
+import {serverSocketAddress} from "../utils/http-communication.js";
+import {useSelector} from "react-redux";
 import Graph from "../graph/graph.js";
 import CommentsGraph from "../graph/commentsGraph";
 
-export default function Admin() {
-  const [connectedUsers, setConnectedUsers] = useState(0);
-  const AdminId = "DY6e4W8aHFMfSMBcDbv3FDBgdEm2";
-  const currentUserID = useSelector((state) => state.user.id);
+export default function Admin(connectedUsers) {
+    const AdminId = "DY6e4W8aHFMfSMBcDbv3FDBgdEm2";
+    const currentUserID = useSelector((state) => state.user.id);
 
-  useEffect(() => {
-    socketInitializtion(setConnectedUsers);
-  }, []);
 
-  return (
-    <div>
-      {currentUserID === AdminId ? (
-        <div className="admin-container">
-          <div className="connected-users-container">
-            <h2 className="connected-users-title">Connected Users:</h2>
-            <p className="connected-users-number">{connectedUsers -1}</p>
-          </div>
-          <div className="chart-container">
-            <Graph></Graph>
-            <CommentsGraph></CommentsGraph>
-          </div>
-          <div>
-
-          </div>
-          <script src="/socket.io/socket.io.js"></script>
+    return (
+        <div>
+            {currentUserID === AdminId ? (
+                <div className="admin-container">
+                    <div className="connected-users-container">
+                        <h2 className="connected-users-title">Connected Users:</h2>
+                        <p className="connected-users-number">{connectedUsers.connectedUsers}</p>
+                    </div>
+                    <div className="chart-container">
+                        <Graph></Graph>
+                        <CommentsGraph></CommentsGraph>
+                    </div>
+                    <div>
+                    </div>
+                </div>
+            ) : (
+                <div className="restricted-message">
+                    You do not have access to this page...
+                </div>
+            )}
         </div>
-      ) : (
-        <div className="restricted-message">
-          You do not have access to this page...
-        </div>
-      )}
-    </div>
-  );
+    );
 }
-function socketInitializtion(setConnectedUsers) {
-  const socket = io(serverSocketAddress);
-  socket.on("counter", (counter) => {
-    setConnectedUsers(counter);
-  });
-  socket.on("connect_error", (error) => {
-    setConnectedUsers("Error");
-    console.log("WS Error: Cannot connect to server");
-  });
-  return () => {
-    socket.disconnect();
-  };
-}
+
 
