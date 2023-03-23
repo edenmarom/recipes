56e3fdd9d3d3f3eac2be1d401ca28528d2cc92ff
Peleg Admi <pelegadmi@gmail.com>
2023-03-23 15:31:52 +0200

    Added Date to about page

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import {serverAddress} from "../utils/http-communication";
 import {initRecipes} from "../Slices/recipeSlice";
 
 const Main = props => {
-const [dateAsNumber,setDateAsNumber] = useState("");
+    const [dateAsNumber, setDateAsNumber] = useState("");
 
     useEffect(() => {
         creationDate();
@@ -13,7 +13,9 @@ const [dateAsNumber,setDateAsNumber] = useState("");
         try {
             let result = await fetch(serverAddress + "/site-info");
             result = await result.json();
-            setDateAsNumber( result.data.date);
+            setDateAsNumber(result.data.date);
+            const x = new Date(dateAsNumber).toDateString()
+            console.log(x)
         } catch (err) {
             console.log("Could not fetch site Info.");
             console.log("Error: " + err);
@@ -21,28 +23,29 @@ const [dateAsNumber,setDateAsNumber] = useState("");
     }
 
     return (
-      <div className="Main">
-        <h1>{props.message}</h1>
-        {props.showbutton === "false" ? (
-          <div>
-              <p>{new Date(dateAsNumber).toDateString()}</p>
-            <p className='dev-header'>Development team:</p>
-            <p>Diana Alankry</p>
-            <p>Chen Atedgi</p>
-            <p>Peleg Admi</p>
-            <p>Eden Marom</p>
-          </div>
-        ) : (
-          <button
-            type="button"
-            className="btn btn-primary btn-lg btn-main"
-            // onClick={() => {
-            //   console.log("signin");
-            // }}
-          >
-          </button>
-        )}
-      </div>
+        <div className="Main">
+            <h1>{props.message}</h1>
+            {props.showbutton === "false" ? (
+                <div>
+                    <p className='dev-header'>Site Creation Date:</p>
+                    <p>{new Date(dateAsNumber).toGMTString()}</p>
+                    <p className='dev-header'>Development team:</p>
+                    <p>Diana Alankry</p>
+                    <p>Chen Atedgi</p>
+                    <p>Peleg Admi</p>
+                    <p>Eden Marom</p>
+                </div>
+            ) : (
+                <button
+                    type="button"
+                    className="btn btn-primary btn-lg btn-main"
+                    // onClick={() => {
+                    //   console.log("signin");
+                    // }}
+                >
+                </button>
+            )}
+        </div>
     );
 }
 
